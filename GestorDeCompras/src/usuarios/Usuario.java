bf5f3d18a8c7a5ac79be5ec14a74d8dc96013f97
gonreynoso <gonzalo.reynoso9@gmail.com>
2025-11-17 22:09:09 -0300

    Cree la clase Usuario con atributos

---
+++ GestorDeCompras/src/usuarios/Usuario.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gonza
+ */
+public class Usuario {
+    public static String nombre;
+    public int numero;
+    
+}
+
